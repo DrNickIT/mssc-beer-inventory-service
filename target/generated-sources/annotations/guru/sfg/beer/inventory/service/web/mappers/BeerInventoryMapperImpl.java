52d7b58a22c3ad34486a451a99ca017a6b8f17f0
Tom Hoornaert <tom.hoornaert@gmail.com>
2019-10-21 13:25:48 +0200

    Fix jackson config

--- target/generated-sources/annotations/guru/sfg/beer/inventory/service/web/mappers/BeerInventoryMapperImpl.java
+++ target/generated-sources/annotations/guru/sfg/beer/inventory/service/web/mappers/BeerInventoryMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2019-10-18T09:56:53+0200",
+    date = "2019-10-21T13:16:13+0200",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 11.0.4 (JetBrains s.r.o)"
 )
 @Component
